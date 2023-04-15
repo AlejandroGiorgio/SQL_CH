bda02f1d5032f51b4adf5cbf4e917acd2910af27
Alejandro Giorgio <112528371+AlejandroGiorgio@users.noreply.github.com>
2023-04-14 23:41:11 -0300

    Update tobbacoNstuff_triggers.sql

--- tobbacoNstuff_triggers.sql
+++ tobbacoNstuff_triggers.sql
@@ -50,3 +50,30 @@ BEGIN
     VALUES (OLD.id_City, OLD.City, CURRENT_USER(), NOW());
 END $$
 DELIMITER ;
+
+-- Este código crea una nueva tabla llamada DeleteCostumerLog con las columnas y tipos de datos especificados.
+CREATE TABLE DeleteCostumerLog (
+	delete_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_Costumer INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	tel INT,
+	BirthDate DATE,
+	id_Province INT,
+	id_City INT,
+    UserName varchar(100) NULL,
+    registerDate timestamp
+);
+
+DELIMITER $$
+-- Este código crea un nuevo disparador llamado DeletedCostumer que se ejecutará antes de una operación DELETE en la tabla tobbaconstuff.costumer.
+-- inserta una nueva fila en la tabla DeleteCostumerLog con los valores de la fila que está a punto de ser eliminada de la tabla tobbaconstuff.costumer.
+CREATE TRIGGER DeletedCostumer
+BEFORE DELETE
+ON tobbaconstuff.costumer FOR EACH ROW
+BEGIN
+    INSERT INTO DeleteCostumerLog (id_Costumer, first_name, last_name, email, tel, BirthDate, id_Province, id_City, UserName, registerDate) 
+    VALUES (OLD.id_Costumer, OLD.first_name, OLD.last_name, OLD.email, OLD.tel, OLD.BirthDate, OLD.id_Province, OLD.id_City, CURRENT_USER(), NOW());
+END $$
+DELIMITER ;
